b4243684878f2120637a48a0911e8bc0303867e8
Quentin Brooks <quentin.brooks@zazzle.com>
2015-09-19 00:40:21 -0400

    Basic manage functionality almost done. Just need to deal with enabling styles.

---
+++ main.js
@@ -0,0 +1,166 @@
+var apply = function(scope) {
+	if(!scope.$root.$$phase) {
+		scope.$apply();
+	}
+};
+
+var mod = angular.module("stylishShare", []);
+mod.controller("rootCtrl", function($scope) {
+	var ref = new Firebase("https://stylish-share.firebaseio.com/");
+	$scope.authenticated = false;
+	$scope.auth;
+	
+	var writeRef;
+	
+	ref.onAuth(function(auth) {
+		$scope.authenticated = !!auth;
+		$scope.auth = auth;
+		if(writeRef) {
+			writeRef.off("value", allOurData);
+		}
+		if(auth) {
+			writeRef = ref.child("userData").child(auth.uid);
+			writeRef.on("value", allOurData);
+		}
+		apply($scope);
+	});
+	$scope.login = function() {
+		ref.authWithOAuthPopup("google", function(error, authData) {
+			if (error) {
+				console.log("Login Failed!", error);
+			} else {
+				console.log("Authenticated successfully with payload:", authData);
+			}
+		});
+	};
+	$scope.logout = function() {
+		ref.unauth();
+	};
+	
+	//Eh... not going to be that much, lets just read it all at once
+	function allOurData(snapshot) {
+		$scope.data = snapshot.val();
+		apply($scope);
+	}
+	
+	function getStyleRef(style) {
+		return writeRef.child("styles").child(style.styleID);
+	}
+	
+	$scope.addStyle = function() {
+		var styleRef = writeRef.child("styles").push();
+		styleRef.child("createDate").set(Firebase.ServerValue.TIMESTAMP);
+		styleRef.child("styleID").set(styleRef.key());
+	};
+	$scope.save = function(style) {
+		var styleRef = getStyleRef(style);
+		styleRef.set(style);
+	};
+	$scope.bindNull1 = function(fnc, arg) {
+		return fnc.bind(null, arg);
+	};
+	$scope.delete = function(style) {
+		if(confirm("Sure you want to delete " + style.content)) {
+			var styleRef = getStyleRef(style);
+			styleRef.remove();
+		}
+	};
+	$scope.lines = function(text) {
+		return text.split("\n").length;	
+	};
+	
+	$scope.addUrl = function(style, value) {
+		var styleRef = getStyleRef(style);
+		styleRef.child("urls").push(value);
+	};
+	
+	$scope.updateUrl = function(style, key, value) {
+		var styleRef = getStyleRef(style);
+		styleRef.child("urls").child(key).set(value);
+	};
+});
+
+mod.directive("cssEditor", function(){
+	return {
+		template: "<div class='code'></div>",
+		scope: {
+			content: "=",
+			style: "=",
+			save: "=",
+			lastSaved: "=?",
+			changed: "=?"
+		},
+		link: function link(scope, elements, attrs) {
+			scope.content = scope.content || "";
+			var lastSavedContent;
+			scope.$watch("lastSaved", function(lastSaved){
+				lastSavedContent = lastSaved;
+				scope.changed = false;
+			});
+			var originalContent = scope.content;
+			scope.$watch("content", function(content){
+				scope.changed = content != originalContent && content != lastSavedContent;
+			});
+			var editor = CodeMirror(elements[0], {
+				mode: "text/css",
+				lineNumbers: true,
+				extraKeys: {
+					"Ctrl-Space": "autocomplete",
+					"Ctrl-S": function() {
+						scope.save(scope.style);
+						scope.lastSaved = scope.content;
+						apply(scope);
+					}
+				},
+				value: originalContent
+			});
+			editor.on("change", function(cm){
+				scope.content = cm.getValue();
+				apply(scope);
+			});
+		}
+	}
+});
+
+//http://eric.sau.pe/angularjs-detect-enter-key-ngenter/
+mod.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
+});
+
+/*
+var editor = CodeMirror(document.getElementById("code"), {
+	mode: "text/css",
+	lineNumbers: true,
+	extraKeys: {"Ctrl-Space": "autocomplete"},
+	value: 'a{}'
+});
+
+editor.setValue("x");
+
+editor.on("keyup", function(x, e) {
+	var keyCode = e.keyCode;
+	//letters
+	if(65 <= keyCode && keyCode <= 90) {
+		CodeMirror.showHint(editor);
+	}
+});
+
+var ref = new Firebase("https://stylish-share.firebaseio.com/");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
+*/
